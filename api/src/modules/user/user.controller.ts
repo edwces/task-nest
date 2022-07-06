1a7356856e7cec761adb54e10d1de12abb0f5ce8
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-07-06 11:28:59 +0200

    get user data by token

--- api/src/modules/user/user.controller.ts
+++ api/src/modules/user/user.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { Roles } from 'src/common/decorators/roles.decorator';
-import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Auth } from 'src/common/decorators/auth.decorator';
+import { User } from 'src/common/decorators/user.decorator';
 import { UserRole } from 'src/common/types/enums/user-role.enum';
-import { AccessGuard } from '../auth/guards/access.guard';
+import { AccessClaims } from '../auth/interfaces/access-claims.interface';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
@@ -11,8 +11,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  @Roles(UserRole.ADMIN)
-  @UseGuards(AccessGuard, RolesGuard)
+  @Auth(UserRole.ADMIN)
   findAll() {
     return this.userService.findAll();
   }
@@ -20,5 +19,11 @@ export class UserController {
   @Post()
   create(@Body() dto: CreateUserDTO) {
     return this.userService.create(dto);
+  }
+
+  @Get('me')
+  @Auth()
+  getByToken(@User() user: AccessClaims) {
+    return this.userService.findOne(user.sub);
   }
 }
--- web/src/modules/todo/services/todo.service.ts
+++ web/src/modules/todo/services/todo.service.ts
@@ -2,9 +2,9 @@ import { http } from "../../../config/httpClient";
 import { AddTodoDTO } from "../dto/add-todo.dto";
 
 export function createTodo(data: AddTodoDTO) {
-  return http.post("todos", { data });
+  return http.post("user/me/todos", { data });
 }
 
 export function getTodos() {
-  return http.get("todos");
+  return http.get("user/me/todos");
 }
