9f9675449aa851eb88183cadf87227b0902583cd
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-07-27 10:45:15 +0200

    add helemt security headers

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
