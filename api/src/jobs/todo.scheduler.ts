b5ddea2150bc756c402bee0f5b650874ab42f404
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-08-16 15:54:45 +0200

    fix cronjobs

--- api/src/jobs/todo.scheduler.ts
+++ api/src/jobs/todo.scheduler.ts
@@ -1,11 +1,11 @@
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityRepository } from '@mikro-orm/postgresql';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, Scope } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { Repeat } from 'src/modules/todo/enums/repeat.enum';
 import { Todo } from 'src/modules/todo/todo.entity';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class TodoScheduler {
   constructor(
     @InjectRepository(Todo)
@@ -15,28 +15,24 @@ export class TodoScheduler {
   @Cron('0 0 * * * *')
   async handleDailyRepeatingTodos() {
     const qb = this.todoRepository.createQueryBuilder();
-
     await qb.update({ isChecked: false }).where({ repeat: Repeat.DAILY });
   }
 
   @Cron(`0 0 * * 0 *`)
   async handleWeeklyRepeatingTodos() {
     const qb = this.todoRepository.createQueryBuilder();
-
     await qb.update({ isChecked: false }).where({ repeat: Repeat.WEEKLY });
   }
 
   @Cron(`0 0 1 * * *`)
   async handleMonthlyRepeatingTodos() {
     const qb = this.todoRepository.createQueryBuilder();
-
     await qb.update({ isChecked: false }).where({ repeat: Repeat.MONTHLY });
   }
 
   @Cron('0 0 * * * *')
   async handleTickingTodos() {
     const qb = this.todoRepository.createQueryBuilder();
-
-    await qb.delete().where({ isChecked: true, repeat: Repeat.NONE });
+    await qb.delete('*').where({ isChecked: true, repeat: Repeat.NONE });
   }
 }
