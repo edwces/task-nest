b4f556f2a779fdfb9ac2f8d76c05975b95971f29
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-08-17 10:02:48 +0200

    fix todo cronjobs

--- api/src/jobs/todo.scheduler.ts
+++ api/src/jobs/todo.scheduler.ts
@@ -1,38 +1,60 @@
-import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/postgresql';
-import { Injectable, Logger, Scope } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { MikroORM, UseRequestContext } from '@mikro-orm/core';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { Repeat } from 'src/modules/todo/enums/repeat.enum';
 import { Todo } from 'src/modules/todo/todo.entity';
 
-@Injectable({ scope: Scope.REQUEST })
+@Injectable()
 export class TodoScheduler {
-  constructor(
-    @InjectRepository(Todo)
-    private readonly todoRepository: EntityRepository<Todo>,
-  ) {}
+  private readonly logger = new Logger(TodoScheduler.name);
 
-  @Cron('0 0 * * * *')
+  constructor(private readonly orm: MikroORM<PostgreSqlDriver>) {}
+
+  @Cron(CronExpression.EVERY_DAY_AT_NOON)
+  @UseRequestContext()
   async handleDailyRepeatingTodos() {
-    const qb = this.todoRepository.createQueryBuilder();
-    await qb.update({ isChecked: false }).where({ repeat: Repeat.DAILY });
+    this.logger.debug('UPDATING daily todos');
+    const qb = this.orm.em.createQueryBuilder(Todo);
+    await qb
+      .update({
+        isChecked: false,
+        expiresAt: qb.raw(`"expires_at" + interval '1 day'`),
+      })
+      .where({ repeat: Repeat.DAILY });
   }
 
-  @Cron(`0 0 * * 0 *`)
+  @Cron(CronExpression.EVERY_WEEK)
+  @UseRequestContext()
   async handleWeeklyRepeatingTodos() {
-    const qb = this.todoRepository.createQueryBuilder();
-    await qb.update({ isChecked: false }).where({ repeat: Repeat.WEEKLY });
+    this.logger.debug('UPDATING weekly todos');
+    const qb = this.orm.em.createQueryBuilder(Todo);
+    await qb
+      .update({
+        isChecked: false,
+        expiresAt: qb.raw(`"expires_at" + interval '7 days'`),
+      })
+      .where({ repeat: Repeat.WEEKLY });
   }
 
-  @Cron(`0 0 1 * * *`)
+  @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT)
+  @UseRequestContext()
   async handleMonthlyRepeatingTodos() {
-    const qb = this.todoRepository.createQueryBuilder();
-    await qb.update({ isChecked: false }).where({ repeat: Repeat.MONTHLY });
+    this.logger.debug('UPDATING monthly todos');
+    const qb = this.orm.em.createQueryBuilder(Todo);
+    await qb
+      .update({
+        isChecked: false,
+        expiresAt: qb.raw(`"expires_at" + interval '1 month'`),
+      })
+      .where({ repeat: Repeat.MONTHLY });
   }
 
-  @Cron('0 0 * * * *')
+  @Cron(CronExpression.EVERY_DAY_AT_NOON)
+  @UseRequestContext()
   async handleTickingTodos() {
-    const qb = this.todoRepository.createQueryBuilder();
+    this.logger.debug('DELETING non-repeating todos');
+    const qb = this.orm.em.createQueryBuilder(Todo);
     await qb.delete('*').where({ isChecked: true, repeat: Repeat.NONE });
   }
 }
